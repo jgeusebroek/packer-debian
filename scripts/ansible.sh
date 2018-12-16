30ff3bfb7f984cbfa3293c4458e8a340c00bcb4a
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2018-12-16 17:27:28 +0100

    Fix sebian 9

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -eux
 
+# Add PPA
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
+
+# Required by Debian 9; otherwise apt-key will fail.
+apt-get -y update dirmngr
+
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
 # Install Ansible dependencies.
