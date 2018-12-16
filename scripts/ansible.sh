ab99403e0d6d91c4dd7167e748a2de9329e22528
Jeroen Geusebroek <me@jeroengeusebroek.nl>
2018-12-16 19:48:21 +0100

    Fix

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -4,7 +4,7 @@
 echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" > /etc/apt/sources.list.d/ansible.list
 
 # Required by Debian 9; otherwise apt-key will fail.
-apt-get -y update dirmngr
+apt-get -y install dirmngr
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 
